a42a24fd610da05ac55e6058528499991eddad60
costagavras <cgavras@yahoo.ca>
2018-12-26 23:03:03 -0500

    eval_attr_accessor

--- advanced_features.rb
+++ advanced_features.rb
@@ -2,13 +2,31 @@
 # an_expression = %{#{a_number}*2}
 # puts eval(an_expression)
 
-def binding_elsewhere
-  x = 20
-  return binding
+# def binding_elsewhere
+#   x = 20
+#   return binding
+# end
+#
+# remote_binding = binding_elsewhere
+#
+# x = 10
+# eval("puts x")
+# eval("puts x", remote_binding)
+
+class Class
+  def add_accessor(accessor_name)
+    self.class_eval %{
+      attr_accessor :#{accessor_name}
+    }
+  end
 end
 
-remote_binding = binding_elsewhere
+class Person
+end
 
-x = 10
-eval("puts x")
-eval("puts x", remote_binding)
+person = Person.new
+Person.add_accessor :name
+Person.add_accessor :gender
+person.name = "Jim Hacker"
+person.gender = "male"
+puts "#{person.name} is #{person.gender}"
